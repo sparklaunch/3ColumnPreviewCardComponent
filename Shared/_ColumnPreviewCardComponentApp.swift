c77d4d0113dd3f8c1e64e3d96cb51470151e17c5
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-25 10:07:45 +0900

    Initial Commit

---
+++ Shared/_ColumnPreviewCardComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  _ColumnPreviewCardComponentApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/03/25.
+//
+
+import SwiftUI
+
+@main
+struct _ColumnPreviewCardComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
